d08861dd5829e4676bc6c9535e8a431ec893ff77
holistic-man <180656535+holistic-man@users.noreply.github.com>
2024-09-12 11:52:24 -0300

    cors_allowed_origins

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="obranet",
     cors_allowed_origins=[
-        "https://obranet.vercel.app/",
+        "https://obranet.vercel.app",
         "http://localhost:3000"
     ]
 )
